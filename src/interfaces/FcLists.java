4827d0fca4b3a6c74ce5f3f3ac659b1e9222a3d0
Acer <Acer@DESKTOP-K09QF87>
2022-04-28 11:02:18 +0200

    Added FcLists.java

---
+++ src/interfaces/FcLists.java
@@ -0,0 +1,12 @@
+package interfaces;
+
+//Font Chooser dialog JLists
+public interface FcLists {
+	String fonts[] = {
+			"Arial" , "Comic Sans MS","Consolas",
+			"Georgia" , "Microsoft Sans Serif", "Montserrat Subrayada",
+			"MS Sans Serif", "Ms Serif", "Roman",
+			"Tahoma", "Times New Roman" ,"Verdana"
+	};
+
+}
