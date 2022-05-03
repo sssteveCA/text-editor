958c5fa6c914c28665dcc75a5580194fd6fe906b
Acer <Acer@DESKTOP-K09QF87>
2022-05-03 15:16:08 +0200

    Updated MyFontChooser.java #3

--- src/classes/events/FcSelectionEvent.java
+++ src/classes/events/FcSelectionEvent.java
@@ -20,23 +20,19 @@ public class FcSelectionEvent implements ListSelectionListener {
 		// TODO Auto-generated method stub
 		//get element when event occurred
 		Object fired = e.getSource();
-		int firstIndex = e.getFirstIndex();
-		int lastIndex = e.getLastIndex();
-		/*System.out.println("firstIndex => "+firstIndex);
-		System.out.println("lastIndex => "+lastIndex);*/
 		if(fired.equals(this.mfc.jlist_font)) {
 			//Value changed in font name list
 			String fontName = this.mfc.jlist_font.getSelectedValue().toString();
-			System.out.println("fontName => "+fontName);
+			this.mfc.jtf_font.setText(fontName);
 		}else if(fired.equals(this.mfc.jlist_style)) {
 			//Value changed in font style list
 			String fontStyle = this.mfc.jlist_style.getSelectedValue().toString();
-			System.out.println("fontStyle => "+fontStyle);
+			this.mfc.jtf_style.setText(fontStyle);
 		}
 		else if(fired.equals(this.mfc.jlist_size)) {
 			//Value changed in font size list
 			String fontSize = this.mfc.jlist_size.getSelectedValue().toString();
-			System.out.println("fontSize => "+fontSize);
+			this.mfc.jtf_size.setText(fontSize);
 		}
 		
 		
