5c7d060897b230f96d1b2613c86b11163a71c7c5
Acer <Acer@DESKTOP-K09QF87>
2022-05-03 15:39:40 +0200

    Updated MyFontChooser.java #4

--- src/classes/events/FcClickEvent.java
+++ src/classes/events/FcClickEvent.java
@@ -1,13 +1,15 @@
 package classes.events;
 
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 import classes.dialogs.MyFontChooser;
 import classes.frames.TextEditor;
+import interfaces.FcLists;
 
 //Click events of MyFontChooser dialog
-public class FcClickEvent implements ActionListener{
+public class FcClickEvent implements ActionListener,FcLists{
 	
 	private MyFontChooser mfc;
 	private TextEditor te;
@@ -35,14 +37,39 @@ public class FcClickEvent implements ActionListener{
 	
 	//Actions when user press OK button in Font Chooser dialog
 	private void okClickActions() {
-		String fontName = this.mfc.jlist_font.getSelectedValue().toString();
-		String fontStyle = this.mfc.jlist_style.getSelectedValue().toString();
-		String fontSize = this.mfc.jlist_size.getSelectedValue().toString();
+		Font font = this.te.textarea.getFont();
+		String fontName = this.mfc.jlist_font.getSelectedValue() != null ? this.mfc.jlist_font.getSelectedValue().toString() : null;
+		String fontStyle = this.mfc.jlist_style.getSelectedValue() != null ? this.mfc.jlist_style.getSelectedValue().toString() : null;
+		String fontSize = this.mfc.jlist_size.getSelectedValue() != null ? this.mfc.jlist_size.getSelectedValue().toString() : null;
 		String writing = this.mfc.jbb_writing.getSelectedItem().toString();
 		System.out.println("fontName => "+fontName);
 		System.out.println("fontStyle => "+fontStyle);
 		System.out.println("fontSize => "+fontSize);
 		System.out.println("writing => "+writing);
+		if(fontStyle != null) {
+			//font style selected in list
+			if(fontStyle.equals(fl_styles[0])) {
+				//Normal style
+				font = font.deriveFont(Font.PLAIN);
+			}
+			else if(fontStyle.equals(fl_styles[1])) {
+				//Italic style
+				font = font.deriveFont(Font.ITALIC);
+			}
+			if(fontStyle.equals(fl_styles[2])) {
+				//Bold style
+				font = font.deriveFont(Font.BOLD);
+			}
+			if(fontStyle.equals(fl_styles[0])) {
+				//Italic bold style
+				font = font.deriveFont(Font.BOLD|Font.ITALIC);
+			}
+		}//if(fontStyle != null) {
+		if(fontSize != null) {
+			//font size selected in list
+			font = font.deriveFont(Float.parseFloat(fontSize));
+		}
+		this.te.textarea.setFont(font);
 	}
 
 }
