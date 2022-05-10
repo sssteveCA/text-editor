f70db500863860e1158d2aceb3c1c8af365cc63c
Acer <Acer@DESKTOP-K09QF87>
2022-05-10 21:48:54 +0200

    Updated main.java
    Added Event Dispath Thread

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import javax.swing.SwingUtilities;
+
 import classes.frames.TextEditor;
 import interfaces.Constants;
 
@@ -5,8 +7,19 @@ public class Main implements Constants {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		TextEditor te = new TextEditor(TE_WINDOW_NAME);
+		SwingUtilities.invokeLater(new Runnable() {
+
+			@Override
+			public void run() {
+				// TODO Auto-generated method stub
+				new Main().createTextEditorFrame();
+			}		
+		});
 	}
 
+	
+	public void createTextEditorFrame() {
+		TextEditor te = new TextEditor(TE_WINDOW_NAME);
+	}
 }
 
