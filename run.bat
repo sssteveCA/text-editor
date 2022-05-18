14ae5ef10fbda59cedf90f503e5bf1255496288e
Acer <Acer@DESKTOP-K09QF87>
2022-05-18 21:59:46 +0200

    Updated run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 path C:\Programmi\Java\jdk1.8.0_211\bin
-javac Main.java
-java Main
+javac -sourcepath C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\ -d C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\bin\ C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\Main.java
+java C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\Main
