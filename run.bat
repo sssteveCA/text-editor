2aaca50cf501c34e84363e5e02becf0affe5dbfe
Acer <Acer@DESKTOP-K09QF87>
2022-05-18 22:00:25 +0200

    .class files staged

--- run.bat
+++
@@ -1,3 +0,0 @@
-path C:\Programmi\Java\jdk1.8.0_211\bin
-javac -sourcepath C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\ -d C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\bin\ C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\Main.java
-java C:\Users\Acer\eclipse-workspace\Text_Editor_Windows\src\Main
