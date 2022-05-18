792702eefdb7d19e7162fa6ce31354a630c9eb8c
Acer <Acer@DESKTOP-K09QF87>
2022-05-18 18:57:08 +0200

    Updated FindTextAction.java
    Added checkSearch method

--- src/classes/FindTextActions.java
+++ src/classes/FindTextActions.java
@@ -1,8 +1,13 @@
 package classes;
 
+import java.awt.Color;
 import java.util.HashMap;
 
 import javax.swing.JTextArea;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.DefaultHighlighter;
+import javax.swing.text.DefaultHighlighter.DefaultHighlightPainter;
+import javax.swing.text.Highlighter;
 
 import interfaces.FtaConstants;
 
@@ -29,6 +34,22 @@ public class FindTextActions implements FtaConstants{
 		
 	}
 	
+	public JTextArea getJtaContent() {return this.jta_content;}
+	public String getSearch() {return this.search;}
+	public String getContent() {return this.content;}
+	public byte getErrno() {return this.errno;}
+	public String getError() {
+		switch(this.errno) {
+			case FTA_UNKNOWNERROR:
+				this.error = Messages.UNKNOWNERROR.toString();
+				break;
+			default:
+				this.error = null;
+				break;
+		}
+		return this.error;
+	}
+	
 	//Set HashMap property values
 	private void setHashmap(HashMap<String, Object> options) {
 		if(options == null)options = new HashMap<String, Object>();
@@ -41,18 +62,58 @@ public class FindTextActions implements FtaConstants{
 			this.options.put("downSelected", options.get("downSelected"));
 		else
 			this.options.put("downSelected", true);
-		
 	}
 	
-	public JTextArea getJtaContent() {return this.jta_content;}
-	public String getSearch() {return this.search;}
-	public String getContent() {return this.content;}
-	public byte getErrno() {return this.errno;}
-	public String getError() {
-		switch(this.errno) {
-			default:
-				this.error = null;
-		}
-		return this.error;
+	//Check the search string in JTextArea content
+	public boolean checkSearch() {
+		boolean ok = false;
+		//JTextArea of parent frame needs the focus
+		this.jta_content.requestFocus();
+		//get JTextArea mark position
+		int markPos = this.jta_content.getCaretPosition();
+		int index = -1;
+		//Check if search direction is downward
+		boolean downSelected = (boolean) this.options.get("downSelected");
+		//Check if the search must be case insentive
+		boolean caseInsensitive = (boolean) this.options.get("caseInsensitive");
+		if(downSelected) {
+			//check if search is case Sensitive
+			if(!caseInsensitive) {
+				index = this.content.toLowerCase().indexOf(this.search.toLowerCase(),markPos);
+			}
+			else {
+				//case insensitive search
+				index = this.content.indexOf(this.search,markPos);
+			}
+		}//if(downSelected) {
+		else {
+			if(!caseInsensitive) {
+				index = this.content.toLowerCase().lastIndexOf(this.search.toLowerCase(),markPos-1);
+			}
+			else {
+				index = this.content.lastIndexOf(this.search,markPos);
+			}
+		}//else di if(downSelected) {
+		if(index > -1) {
+			//Substring found
+			//Get length of search string
+			int searchLen = this.search.length();
+			int start = index;
+			//Underline the substring search found in text
+			int end = index+searchLen;
+			this.jta_content.setSelectionColor(Color.BLUE);
+			Highlighter hg = this.jta_content.getHighlighter();
+			hg.removeAllHighlights();
+			this.jta_content.setHighlighter(hg);
+			try {
+				this.jta_content.getHighlighter().addHighlight(start, end, new DefaultHighlighter.DefaultHighlightPainter(Color.LIGHT_GRAY));
+			} catch (BadLocationException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				this.errno = FTA_UNKNOWNERROR;
+			}
+		}//if(index > -1) {
+		else this.errno = FTA_SEARCHNOTFOUND;
+		return ok;
 	}
 }
--- src/interfaces/FtaConstants.java
+++ src/interfaces/FtaConstants.java
@@ -3,10 +3,27 @@ package interfaces;
 //Constants used by TextFindActions
 public interface FtaConstants {
 	
+	//error number constants
+	byte FTA_SEARCHNOTFOUND = 1;
+	byte FTA_UNKNOWNERROR = 2;
+	
+	//error string messages
+	public enum Messages{
+		UNKNOWNERROR("Errore durante la selezione della stringa cercata");
+		
+		private final String msg;
+		
+		Messages(final String msg){this.msg = msg;}
+		
+		@Override
+		public String toString() {return this.msg;}
+	}
+	
+	
 	//Message exceptions
 	public enum Exceptions{
 		JTA_NULL("L'istanza di JTextArea è null"), //JTextArea instance is null
-		SEARCH_NULL(""); //Search string is null
+		SEARCH_NULL("Nessun testo da cercare inserito"); //Search string is null
 		
 		private final String msg;
 		
