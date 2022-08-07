3a15f927493bfc67fede096a3e71b8c2570f4c7f
Acer <Acer@DESKTOP-K09QF87>
2022-08-07 08:48:22 +0200

    Added log4j debug in Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,19 @@
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Properties;
+
 import javax.swing.SwingUtilities;
+
+import org.apache.log4j.Level;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 
 import classes.frames.TextEditor;
 import interfaces.Constants;
 
 public class Main implements Constants {
+	
+	final static Logger log = Logger.getLogger("Main");
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
@@ -12,7 +22,16 @@ public class Main implements Constants {
 			@Override
 			public void run() {
 				// TODO Auto-generated method stub
-				new Main().createTextEditorFrame();
+				Properties prop = new Properties();
+				try {
+					prop.load((Main.class).getResourceAsStream("./log4j.properties"));
+					PropertyConfigurator.configure(prop);
+					log.setLevel(Level.ALL);
+					new Main().createTextEditorFrame();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}				
 			}		
 		});
 	}
