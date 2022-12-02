5a475843a407edf6f8aababf6c59de96b81b5d81
Acer <Acer@DESKTOP-K09QF87>
2022-12-02 08:53:44 +0100

    Added submethods for menu() to TextEditor.java

--- src/classes/frames/TextEditor.java
+++ src/classes/frames/TextEditor.java
@@ -28,7 +28,7 @@ public class TextEditor extends JFrame implements Constants,MenuVals{
 	public JMenuItem miAutoWrap; //Needed for change the label on click
 	public JTextArea textarea;
 	//Right click 'Edit' popup menu
-	public final JPopupMenu pm_edit = new JPopupMenu(TE_PM1);
+	public JPopupMenu pm_edit;
 	public String searchString = null; //Search string passed from Text Find dialog
 	public boolean caseInsensitive = false; //String search sensitive option from Text Find dialog(default false)
 	
@@ -54,92 +54,17 @@ public class TextEditor extends JFrame implements Constants,MenuVals{
 	 */
 	private JMenuBar menu() {
 		JMenuBar mb = new JMenuBar();
-			JMenu mFile = new JMenu(Menu.FILE.toString());
-				JMenuItem miNew = new JMenuItem(Menu.mFile.NEW.toString());
-				JMenuItem miOpen = new JMenuItem(Menu.mFile.OPEN.toString());
-				JMenuItem miSave = new JMenuItem(Menu.mFile.SAVE.toString());
-				JMenuItem miPrint = new JMenuItem(Menu.mFile.PRINT.toString());
-				JMenuItem miExit = new JMenuItem(Menu.mFile.EXIT.toString());
-				miNew.addActionListener(new TeClickEvent(this));
-				miOpen.addActionListener(new TeClickEvent(this));
-				miSave.addActionListener(new TeClickEvent(this));
-				miPrint.addActionListener(new TeClickEvent(this));
-				miExit.addActionListener(new TeClickEvent(this));
-			mFile.add(miNew);
-			mFile.add(miOpen);
-			mFile.add(miSave);
-			mFile.addSeparator();
-			mFile.add(miPrint);
-			mFile.addSeparator();
-			mFile.add(miExit);
+		JMenu mFile = this.menuFile();
 		mb.add(mFile);
-			JMenu mEdit = new JMenu(Menu.EDIT.toString());
-				JMenuItem miCut = new JMenuItem(Menu.mEdit.CUT.toString());
-				JMenuItem miCopy = new JMenuItem(Menu.mEdit.COPY.toString());
-				JMenuItem miPaste = new JMenuItem(Menu.mEdit.PASTE.toString());
-				JMenuItem miFind = new JMenuItem(Menu.mEdit.FIND.toString());
-				JMenuItem miFindPre = new JMenuItem(Menu.mEdit.FIND_PRE.toString());
-				JMenuItem miFindNext = new JMenuItem(Menu.mEdit.FIND_NEXT.toString());
-				JMenuItem miSelectAll = new JMenuItem(Menu.mEdit.SELECT_ALL.toString());
-				miCut.addActionListener(new TeClickEvent(this));
-				miCopy.addActionListener(new TeClickEvent(this));
-				miPaste.addActionListener(new TeClickEvent(this));
-				miFind.addActionListener(new TeClickEvent(this));
-				miFindPre.addActionListener(new TeClickEvent(this));
-				miFindNext.addActionListener(new TeClickEvent(this));
-				miSelectAll.addActionListener(new TeClickEvent(this));
-			mEdit.add(miCut);
-			mEdit.add(miCopy);
-			mEdit.add(miPaste);
-			mEdit.addSeparator();
-			mEdit.add(miFind);
-			mEdit.add(miFindPre);
-			mEdit.add(miFindNext);
-			mEdit.addSeparator();
-			mEdit.add(miSelectAll);
+		JMenu mEdit = this.menuEdit();
 		mb.add(mEdit);
-			JMenu mFormat = new JMenu(Menu.FORMAT.toString());
-				this.miAutoWrap = new JMenuItem(Menu.mFormat.AUTO_WRAP.toString());
-				JMenuItem miFont = new JMenuItem(Menu.mFormat.FONT.toString());
-				this.miAutoWrap.addActionListener(new TeClickEvent(this));
-				miFont.addActionListener(new TeClickEvent(this));
-			mFormat.add(miAutoWrap);
-			mFormat.add(miFont);
+		JMenu mFormat = this.menuFormat();
 		mb.add(mFormat);
-			JMenu mView = new JMenu(Menu.VIEW.toString());
-				JMenu mZoom = new JMenu(Menu.mView.ZOOM.toString());
-					JMenuItem mZoomIn = new JMenuItem(Menu.mView.mZoom.ZOOM_IN.toString());
-					JMenuItem mZoomOut = new JMenuItem(Menu.mView.mZoom.ZOOM_OUT.toString());
-					JMenuItem mDefaultZoom = new JMenuItem(Menu.mView.mZoom.DEFAULT_ZOOM.toString());
-					mZoomIn.addActionListener(new TeClickEvent(this));
-					mZoomOut.addActionListener(new TeClickEvent(this));
-					mDefaultZoom.addActionListener(new TeClickEvent(this));
-				mZoom.add(mZoomIn);
-				mZoom.add(mZoomOut);
-				mZoom.add(mDefaultZoom);
-				mView.add(mZoom);
-				JMenuItem mStatusBar = new JMenuItem(Menu.mView.STATUS_BAR.toString());
-				mStatusBar.addActionListener(new TeClickEvent(this));
-			mView.add(mStatusBar);
+		JMenu mView = this.menuView();
 		mb.add(mView);
-			JMenu mAbout = new JMenu(Menu.ABOUT.toString());
-				JMenuItem mAboutTe = new JMenuItem(Menu.mAbout.ABOUT_TE.toString());
-				mAboutTe.addActionListener(new TeClickEvent(this));
-			mAbout.add(mAboutTe);
+		JMenu mAbout = this.menuAbout();
 		mb.add(mAbout);
-			JMenuItem pm_cut = new JMenuItem(PopupVals.popMenu.CUT.toString());
-			JMenuItem pm_copy = new JMenuItem(PopupVals.popMenu.COPY.toString());
-			JMenuItem pm_paste = new JMenuItem(PopupVals.popMenu.PASTE.toString());
-			JMenuItem pm_selectAll = new JMenuItem(PopupVals.popMenu.SELECT_ALL.toString());
-			pm_cut.addActionListener(new TeClickEvent(this));
-			pm_copy.addActionListener(new TeClickEvent(this));
-			pm_paste.addActionListener(new TeClickEvent(this));
-			pm_selectAll.addActionListener(new TeClickEvent(this));
-			this.pm_edit.add(pm_cut);
-			this.pm_edit.add(pm_copy);
-			this.pm_edit.add(pm_paste);
-			this.pm_edit.addSeparator();
-			this.pm_edit.add(pm_selectAll);
+		this.pm_edit = this.rightClickMenu();
 		return mb;
 	}
 	
@@ -153,5 +78,134 @@ public class TextEditor extends JFrame implements Constants,MenuVals{
 			str+=line+"\n";
 		}
 		return str;
+	}
+
+	/**
+	 * Dropdown menu 'About'
+	 * @return 
+	 */
+	private JMenu menuAbout(){
+		JMenu mAbout = new JMenu(Menu.ABOUT.toString());
+		JMenuItem mAboutTe = new JMenuItem(Menu.mAbout.ABOUT_TE.toString());
+		mAboutTe.addActionListener(new TeClickEvent(this));
+		mAbout.add(mAboutTe);
+		return mAbout;
+	}
+
+	/**
+	 * Dropdown menu 'Edit'
+	 * @return
+	 */
+	private JMenu menuEdit(){
+		JMenu mEdit = new JMenu(Menu.EDIT.toString());
+		JMenuItem miCut = new JMenuItem(Menu.mEdit.CUT.toString());
+		JMenuItem miCopy = new JMenuItem(Menu.mEdit.COPY.toString());
+		JMenuItem miPaste = new JMenuItem(Menu.mEdit.PASTE.toString());
+		JMenuItem miFind = new JMenuItem(Menu.mEdit.FIND.toString());
+		JMenuItem miFindPre = new JMenuItem(Menu.mEdit.FIND_PRE.toString());
+		JMenuItem miFindNext = new JMenuItem(Menu.mEdit.FIND_NEXT.toString());
+		JMenuItem miSelectAll = new JMenuItem(Menu.mEdit.SELECT_ALL.toString());
+		miCut.addActionListener(new TeClickEvent(this));
+		miCopy.addActionListener(new TeClickEvent(this));
+		miPaste.addActionListener(new TeClickEvent(this));
+		miFind.addActionListener(new TeClickEvent(this));
+		miFindPre.addActionListener(new TeClickEvent(this));
+		miFindNext.addActionListener(new TeClickEvent(this));
+		miSelectAll.addActionListener(new TeClickEvent(this));
+		mEdit.add(miCut);
+		mEdit.add(miCopy);
+		mEdit.add(miPaste);
+		mEdit.addSeparator();
+		mEdit.add(miFind);
+		mEdit.add(miFindPre);
+		mEdit.add(miFindNext);
+		mEdit.addSeparator();
+		mEdit.add(miSelectAll);
+		return mEdit;
+	}
+
+	/**
+	 * Dropdown menu 'Format'
+	 * @return
+	 */
+	private JMenu menuFormat(){
+		JMenu mFormat = new JMenu(Menu.FORMAT.toString());
+		this.miAutoWrap = new JMenuItem(Menu.mFormat.AUTO_WRAP.toString());
+		JMenuItem miFont = new JMenuItem(Menu.mFormat.FONT.toString());
+		this.miAutoWrap.addActionListener(new TeClickEvent(this));
+		miFont.addActionListener(new TeClickEvent(this));
+		mFormat.add(miAutoWrap);
+		mFormat.add(miFont);
+		return mFormat;
+	}
+
+	/**
+	 * Dprodown menu 'File'
+	 */
+	private JMenu menuFile(){
+		JMenu mFile = new JMenu(Menu.FILE.toString());
+		JMenuItem miNew = new JMenuItem(Menu.mFile.NEW.toString());
+		JMenuItem miOpen = new JMenuItem(Menu.mFile.OPEN.toString());
+		JMenuItem miSave = new JMenuItem(Menu.mFile.SAVE.toString());
+		JMenuItem miPrint = new JMenuItem(Menu.mFile.PRINT.toString());
+		JMenuItem miExit = new JMenuItem(Menu.mFile.EXIT.toString());
+		miNew.addActionListener(new TeClickEvent(this));
+		miOpen.addActionListener(new TeClickEvent(this));
+		miSave.addActionListener(new TeClickEvent(this));
+		miPrint.addActionListener(new TeClickEvent(this));
+		miExit.addActionListener(new TeClickEvent(this));
+		mFile.add(miNew);
+		mFile.add(miOpen);
+		mFile.add(miSave);
+		mFile.addSeparator();
+		mFile.add(miPrint);
+		mFile.addSeparator();
+		mFile.add(miExit);
+		return mFile;
+	}
+
+	/**
+	 * Dropdown menu 'View'
+	 * @return
+	 */
+	private JMenu menuView(){
+		JMenu mView = new JMenu(Menu.VIEW.toString());
+		JMenu mZoom = new JMenu(Menu.mView.ZOOM.toString());
+		JMenuItem mZoomIn = new JMenuItem(Menu.mView.mZoom.ZOOM_IN.toString());
+		JMenuItem mZoomOut = new JMenuItem(Menu.mView.mZoom.ZOOM_OUT.toString());
+		JMenuItem mDefaultZoom = new JMenuItem(Menu.mView.mZoom.DEFAULT_ZOOM.toString());
+		mZoomIn.addActionListener(new TeClickEvent(this));
+		mZoomOut.addActionListener(new TeClickEvent(this));
+		mDefaultZoom.addActionListener(new TeClickEvent(this));
+		mZoom.add(mZoomIn);
+		mZoom.add(mZoomOut);
+		mZoom.add(mDefaultZoom);
+		mView.add(mZoom);
+		JMenuItem mStatusBar = new JMenuItem(Menu.mView.STATUS_BAR.toString());
+		mStatusBar.addActionListener(new TeClickEvent(this));
+		mView.add(mStatusBar);
+		return mView;
+	}
+
+	/**
+	 * Popup menu shown when user clicks the right button
+	 * @return
+	 */
+	private JPopupMenu rightClickMenu(){
+		JPopupMenu pm_edit = new JPopupMenu(TE_PM1);
+		JMenuItem pm_cut = new JMenuItem(PopupVals.popMenu.CUT.toString());
+		JMenuItem pm_copy = new JMenuItem(PopupVals.popMenu.COPY.toString());
+		JMenuItem pm_paste = new JMenuItem(PopupVals.popMenu.PASTE.toString());
+		JMenuItem pm_selectAll = new JMenuItem(PopupVals.popMenu.SELECT_ALL.toString());
+		pm_cut.addActionListener(new TeClickEvent(this));
+		pm_copy.addActionListener(new TeClickEvent(this));
+		pm_paste.addActionListener(new TeClickEvent(this));
+		pm_selectAll.addActionListener(new TeClickEvent(this));
+		pm_edit.add(pm_cut);
+		pm_edit.add(pm_copy);
+		pm_edit.add(pm_paste);
+		pm_edit.addSeparator();
+		pm_edit.add(pm_selectAll);
+		return pm_edit;
 	}
 }
